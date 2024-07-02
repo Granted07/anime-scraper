70c36e124e182ac8643cc10af5f481fd8ddc878a
Granted07 <anjishnu.04032007@gmail.com>
2024-07-02 23:05:39 +0530

    download link prints

--- episodes.py
+++ episodes.py
@@ -1,12 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+from download import get_download_link
 
-def download_anime(gogolink):
+
+def find_ep(gogolink):
+    global r
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:110.0) Gecko/20100101 Firefox/110.0.'}
-    r = requests.get(gogolink,timeout=10)
+    f = True
+    while f:
+        try:
+            r = requests.get(gogolink, timeout=10, headers=headers)
+            f = 0
+        except:
+            continue
     # requests.session().close()
     soup = BeautifulSoup(r.content, 'html.parser')
-    eps = soup.find_all('div', class_='name')
-    for i in eps:
-        print(i.get_text())
+    eps = soup.find_all('div', class_='anime_video_body')[0].find('a').getText().split('-')
+    get_download_link(int(input(f'Enter Episode ({int(eps[0])+1}-{int(eps[0])+1}): ')),gogolink)
+    # open('test.html', 'w', encoding="utf-8").write(soup.prettify())
