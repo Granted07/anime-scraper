6330e1910c1b8919a2352c59566796932ca8fa47
Granted07 <anjishnu.04032007@gmail.com>
2024-07-03 00:43:43 +0530

    download link navigation pt1

--- download.py
+++ download.py
@@ -1,10 +1,18 @@
+import time
+# import selenium
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 
-def get_download_link(epNumber,gogolink):
+def get_download_link(epNumber, gogolink):
     global search_source
-    link = gogolink.replace('category/','') + '-episode-'+ str(epNumber)
+    link = gogolink.replace('category/', '') + '-episode-' + str(epNumber)
     print(link)
     headers = {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
@@ -19,5 +27,39 @@ def get_download_link(epNumber,gogolink):
     downlink = soup.find('li', class_='dowloads').find('a')['href']
     open('test.html', 'w', encoding='utf-8').write(str(downlink))
     print(downlink)
+    download_episode(downlink)
 
 
+def download_episode(link,):
+    global j
+    dowkeys, dowvalues = [], []
+    options = webdriver.FirefoxOptions()
+    # options.add_argument("-profile")
+    options.add_argument("-headless")
+    driver = webdriver.Firefox(options=options)
+    # wait = WebDriverWait(driver, 100)
+    driver.get(link)
+    html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+    driver.quit()
+    # print(html)
+    open('test.html', 'w', encoding='utf-8').write(str(html))
+    # f = True
+    # while f:
+    #     try:
+    #         s = requests.Session()
+    #         search_source = s.get(link, timeout=10, allow_redirects=True)
+    #         time.sleep(5)
+    #         f = 0
+    #     except:
+    #         continue
+    soup = BeautifulSoup(html, 'html.parser')
+    i = soup.findAll('div', class_='dowload')
+    for j in i:
+        dowkeys.append(j.find('a').text.split())
+        dowvalues.append(j.find('a').attrs['href'])
+    dowdict = dict(zip(dowkeys, dowvalues))
+    for i in dowdict:
+        print(f"{i}= {dowdict[i]}")
+    # print(dowclass[0])
+
+    # print(downlink, "\n", type(downlink))
--- episodes.py
+++ episodes.py
@@ -17,5 +17,5 @@ def find_ep(gogolink):
     # requests.session().close()
     soup = BeautifulSoup(r.content, 'html.parser')
     eps = soup.find_all('div', class_='anime_video_body')[0].find('a').getText().split('-')
-    get_download_link(int(input(f'Enter Episode ({int(eps[0])+1}-{int(eps[0])+1}): ')),gogolink)
+    get_download_link(int(input(f'Enter Episode ({int(eps[0])+1}-{int(eps[1])}): ')),gogolink)
     # open('test.html', 'w', encoding="utf-8").write(soup.prettify())
